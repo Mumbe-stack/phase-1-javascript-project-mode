910dd5367dd870ad7108763628cd08a6755d6727
Mercy <mercymumbe17@gmail.com>
2025-03-25 12:23:03 +0300

    added books jpg files

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-fetch 
+fetch ("http://localhost:3000/books")
+
+.then(response => response.json)
+
+.then(data => {
+    data.forEach(book => {
+        
+    });
+})
+
+.catch(error=> error)
+
+
+function displayBooks(book) {
+    document.getElementById("book-name").textContent = book.name;
+    document.getElementById("book-image").src = book.image;
+    document.getElementById("book-author").textContent = book.author;
+    document.getElementById("book-pages").textContent = book.pages;
+    document.getElementById("publication-year").textContent = publication.year
+
+
+    
+}
